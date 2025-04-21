8a5f26018b37e69954cb2a92f76ca87ba3e54613
Dimakatso Matlaila <dkmatlaila@gmail.com>
2025-04-21 15:25:39 +0200

    Fix: Hardcoded port

--- api_server.py
+++ api_server.py
@@ -252,6 +252,6 @@ class BroadcastEmail(Resource):
 
 # Register namespace
 api.add_namespace(ns)
-
+Port = os.getenv("PORT")
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=4000)
+    app.run(debug=True, host='0.0.0.0', port=Port)
